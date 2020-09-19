0e96cfcfb0cb2c1d8493b6c73d827b39842b5e6f
naraoka2329 <naraoka2329@gmail.com>
2020-09-19 22:33:53 +0900

    tutrial_extemsions_done

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'movie.apps.BlogConfig',
     'movie',#追加
 ]
 
@@ -120,3 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
